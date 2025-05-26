450b9af1aaef1b4d502aa92fa6adc7df6bb4e944
Daniele <daniele.tramonti2@studio.unibo.it>
2025-05-26 15:23:37 +0200

    added first files.java

---
+++ app/src/main/java/db_ass/data/DAOException.java
@@ -0,0 +1,22 @@
+package app.src.main.java.db_ass.data;
+
+import java.io.Serial;
+
+public final class DAOException extends RuntimeException{
+    
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(Throwable cause) {
+        super(cause);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ app/src/main/java/db_ass/data/DAOUtils.java
@@ -0,0 +1,36 @@
+package app.src.main.java.db_ass.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public final class DAOUtils {
+    
+    public static Connection localMySQLConnection(String database, String username, String password) {
+        try {
+            var host = "localhost";
+            var port = "3306";
+            var connectionString = "jdbc:mysql://" + host + ":" + port + "/" + database;
+            return DriverManager.getConnection(connectionString, username, password);
+        } catch (Exception e) {
+            throw new DAOException(e);
+        }
+
+        public static PreparedStatement prepare(Connection connection, String query, Object... values) throws SQLException {
+            PreparedStatement statement = null;
+            try {
+                statement = connection.prepareStatement(query);
+                for (int i = 0; i < values.lenght; i++) {
+                    statement.setObject(i + 1, values[i]);
+                }
+                return statement;
+            } catch (Exception e) {
+                if (statement != NULL) {
+                    statement.close();
+                }
+                throw e;
+            }
+        }
+    }
+}

2e3940abd18e95576008edf59d5c8360e9dd9dad
ravag <alerava4@gmail.com>
2025-05-28 11:36:00 +0200

    Added fifth query

--- app/src/main/java/db_ass/data/Queries.java
+++ app/src/main/java/db_ass/data/Queries.java
@@ -26,7 +26,7 @@ public final class Queries {
             from campo
             where tipo = ?
             and NumeroCampo not in (SELECT c.NumeroCampo
-						            FROM prenotazione p, fascia_oraria f, campo c
+				    FROM prenotazione p, fascia_oraria f, campo c
                                     WHERE p.NumeroCampo = f.NumeroCampo
                                     AND f.NumeroCampo = c.NumeroCampo
                                     AND f.Tipo = "Prenotabile"
@@ -41,4 +41,61 @@ public final class Queries {
             INSERT INTO prenotazione(NumeroCampo, Giorno, OrarioInizio, DataPrenotazioneEffettuata, DataPartita, Prenotante)
             VALUES (?, ?, ?, ?, ?, ?);        
             """;
+
+	public static final String FIND_JOINABLE_LESSON = 
+			"""
+			SELECT l.Giorno, l.NumeroCampo, l.OrarioInizio
+			FROM fascia_oraria f join lezione_privata l on (f.NumeroCampo = l.NumeroCampo and f.Giorno = l.Giorno and f.OrarioInizio = l.OrarioInizio)
+			WHERE l.DataSvolgimento = ?
+			AND f.OrarioInizio = ?
+			AND l.SportPraticato = ?
+			AND f.Tipo = "Lezione"
+			AND (l.Partecipante3 is NULL OR l.Partecipante2 is NULL);		
+			""";
+
+	public static final String JOIN_LESSON_AS_SECOND_PARTECIPANT = 
+			"""
+			UPDATE lezione_privata 
+			SET Partecipante2 = ? 
+			WHERE NumeroCampo = ?
+			AND Giorno = ?
+			AND OrarioInizio = ?
+			AND DataSvolgimento = ?
+			AND SportPraticato = ?
+			AND Partecipante2 is null;
+			""";
+
+	public static final String JOIN_LESSON_AS_THIRD_PARTECIPANT = 
+			"""
+			UPDATE lezione_privata 
+			SET Partecipante3 = ?
+			WHERE NumeroCampo = ?
+			AND Giorno = ?
+			AND OrarioInizio = ?
+			AND DataSvolgimento = ?
+			AND SportPraticato = ?
+			AND Partecipante3 is null;		
+			""";
+	
+	public static final String FIND_SPACE_FOR_NEW_LESSON = 
+			"""
+			select c.NumeroCampo
+			from campo c
+			where Tipo = ?
+			and NumeroCampo not in (select f.NumeroCampo
+									from fascia_oraria f left join lezione_privata lp on (f.OrarioInizio = lp.OrarioInizio and f.Giorno = lp.Giorno and f.NumeroCampo = lp.NumeroCampo) 
+									left join lezione_corso lc on (f.OrarioInizio = lc.OrarioInizio and f.Giorno = lc.Giorno and f.NumeroCampo = lc.NumeroCampo) 
+									where f.OrarioInizio = ?
+									and f.Giorno = ?
+									and (lp.DataSvolgimento = ? or lc.DataSvolgimento = ?));		
+			""";
+
+	public static final String CREATE_NEW_LESSON = 
+			"""
+			INSERT INTO lezione_privata(NumeroCampo, Giorno, OrarioInizio, DataSvolgimento, SportPraticato, Prezzo, Allenatore, Partecipante1)
+			VALUES (?,?,?,?,?,?,?,?);
+			UPDATE persona
+			SET LezioniTenute = LezioniTenute + 1
+			WHERE CF = ?;		
+			""";
 }

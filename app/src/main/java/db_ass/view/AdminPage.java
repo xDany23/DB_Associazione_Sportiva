fd05e87251d80cd0e297b16f298fe34e7a2ebd72
ravag <alerava4@gmail.com>
2025-05-31 12:39:22 +0200

    Added panels to AdminPage

--- app/src/main/java/db_ass/view/AdminPage.java
+++ app/src/main/java/db_ass/view/AdminPage.java
@@ -8,6 +8,7 @@ import java.awt.event.WindowListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -28,8 +29,35 @@ public class AdminPage {
     }
 
     public JFrame setUp() {
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        JPanel userPanel = new JPanel();
+        userPanel.setLayout(new BoxLayout(userPanel, BoxLayout.Y_AXIS));
+
+        this.panel.addTab("Utenti", userPanel);
+
+        JPanel trainerPanel = new JPanel();
+        trainerPanel.setLayout(new BoxLayout(trainerPanel, BoxLayout.Y_AXIS));
+
+        this.panel.addTab("Allenatori", trainerPanel);
+
+        JPanel refereePanel = new JPanel();
+        refereePanel.setLayout(new BoxLayout(refereePanel, BoxLayout.Y_AXIS));
+        
+        this.panel.addTab("Arbitri", refereePanel);
+        
+        JPanel coursePanel = new JPanel();
+        coursePanel.setLayout(new BoxLayout(coursePanel, BoxLayout.Y_AXIS));
+
+        this.panel.addTab("Corsi", coursePanel);
+        
+        JPanel tournamentPanel = new JPanel();
+        tournamentPanel.setLayout(new BoxLayout(tournamentPanel, BoxLayout.Y_AXIS));
+
+        this.panel.addTab("Tornei", tournamentPanel);
+
+        JPanel fieldPanel = new JPanel();
+        fieldPanel.setLayout(new BoxLayout(fieldPanel, BoxLayout.Y_AXIS));
+
+        this.panel.addTab("Campi", fieldPanel);
 
         JPanel backPanel = new JPanel();
         backPanel.setLayout(new BoxLayout(backPanel, BoxLayout.Y_AXIS));
@@ -43,9 +71,10 @@ public class AdminPage {
 
 
         //aggiungo tutto al mainFrame
-        /* mainFrame.getContentPane().add(mainPanel, BorderLayout.CENTER);
-        mainFrame.getContentPane().add(backPanel, BorderLayout.WEST); */
-        mainFrame.getContentPane().add(this.panel);
+        /* mainFrame.getContentPane().add(mainPanel, BorderLayout.CENTER);*/
+        mainFrame.getContentPane().add(new JLabel("Benvenuto " + persona.nome), BorderLayout.NORTH);
+        mainFrame.getContentPane().add(backPanel, BorderLayout.WEST);
+        mainFrame.getContentPane().add(this.panel, BorderLayout.CENTER);
         mainFrame.setResizable(false);
         mainFrame.setVisible(true);
         mainFrame.addWindowListener(
@@ -56,10 +85,7 @@ public class AdminPage {
                 }
             }
         );
-
-        this.panel.addTab("Indietro", backPanel);
-        this.panel.addTab("Principale", mainPanel);
-
+        
         return mainFrame;
     }
 
